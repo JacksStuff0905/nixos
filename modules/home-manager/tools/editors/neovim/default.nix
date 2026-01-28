3500b0dbd12bba2bcef978c3b4066c2cdd1c5ab6
Jacek Sawiński <jacek.sawinski.0905@gmail.com>
2026-01-28 19:48:58 +0100

    Refactor

--- modules/home-manager/tools/editors/neovim/default.nix
+++ modules/home-manager/tools/editors/neovim/default.nix
@@ -1,52 +1,62 @@
-{config, pkgs, lib, inputs, ...}:
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}:
 
 let
-        available-themes = if (builtins.pathExists ./themes) then builtins.attrNames (builtins.readDir ./themes) else [];
+  available-themes =
+    if (builtins.pathExists ./themes) then builtins.attrNames (builtins.readDir ./themes) else [ ];
 
-        cfg = config.tools.editors.neovim;
+  cfg = config.tools.editors.neovim;
 in
 {
-        imports = [
-                inputs.nvim-nix.homeManagerModules.default
+  imports = [
+    inputs.nvim-nix.homeManagerModules.default
+  ];
+
+  options.tools.editors.neovim = {
+    enable = lib.mkEnableOption "Enable the nvim-nix managment module";
+
+    profile = lib.mkOption {
+      type = lib.types.str;
+      default = "full";
+    };
+
+    theme = {
+      name = lib.mkOption {
+        type = lib.types.str;
+        default = config.themes.theme.name;
+      };
+
+      style = lib.mkOption {
+        type = lib.types.enum [
+          "dark"
+          "light"
         ];
+        default = config.themes.theme.style;
+      };
+    };
+  };
 
-        options.tools.editors.neovim = {
-                enable = lib.mkEnableOption "Enable the nvim-nix managment module";
+  config.programs.nvim-nix = lib.mkIf cfg.enable {
+    enable = true;
 
-                profile = lib.mkOption {
-                        type = lib.types.str;
-                        default = "full";
-                };
+    profile = cfg.profile;
 
-                theme = {
-                        name = lib.mkOption {
-                                type = lib.types.str;
-                                default = config.themes.theme.name;
-                        };
+    themes = {
+      enable = true;
 
-                        style = lib.mkOption {
-                                type = lib.types.enum ["dark" "light"];
-                                default = config.themes.theme.style;
-                        };
-                };
-        };
-        
-        config.programs.nvim-nix = lib.mkIf cfg.enable {
-                enable = true;
+      theme = {
+        name = cfg.theme.name;
+        style = cfg.theme.style;
 
-                profile = cfg.profile;
-
-                themes = {
-                        enable = true;
-
-                        theme = {
-                                name = cfg.theme.name;
-                                style = cfg.theme.style;
-
-                                path = (if (builtins.elem cfg.theme.name available-themes) then
-                                        ./themes + ("/" + cfg.theme.name)
-                                else null);
-                        };
-                };
-        };
+        path = (
+          if (builtins.elem cfg.theme.name available-themes) then ./themes + ("/" + cfg.theme.name) else null
+        );
+      };
+    };
+  };
 }
