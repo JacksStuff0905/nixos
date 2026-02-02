6165742f81c144ac4b287b5367c4489052559c8c
Jacek Sawiński <jacek.sawinski.0905@gmail.com>
2026-02-02 22:11:04 +0100

    Reformat

--- modules/home-manager/themes/default.nix
+++ modules/home-manager/themes/default.nix
@@ -1,23 +1,32 @@
-{config, lib, pkgs, util, ...}:
+{
+  config,
+  lib,
+  pkgs,
+  util,
+  ...
+}:
 
 let
-        file_to_not_import = [
-                "default.nix"
-        ];
+  file_to_not_import = [
+    "default.nix"
+  ];
 in
 {
-        options.themes = {
-                enable = lib.mkEnableOption "Enable theme module";
-                theme = {
-                        name = lib.mkOption {
-                                type = lib.types.str;
-                                default = "godot";
-                        };
-                        
-                        style = lib.mkOption {
-                                type = lib.types.enum ["dark" "light"];
-                                default = "dark";
-                        };
-                };
-        };
+  options.themes = {
+    enable = lib.mkEnableOption "Enable theme module";
+    theme = {
+      name = lib.mkOption {
+        type = lib.types.str;
+        default = "godot";
+      };
+
+      style = lib.mkOption {
+        type = lib.types.enum [
+          "dark"
+          "light"
+        ];
+        default = "dark";
+      };
+    };
+  };
 }
