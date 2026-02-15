a6e978cde5ea5b09a882a0f347a13d090e205f10
Jacek Sawiński <jacek.sawinski.0905@gmail.com>
2026-02-15 13:58:44 +0100

    Added stylix

--- modules/home-manager/themes/default.nix
+++ modules/home-manager/themes/default.nix
@@ -3,20 +3,34 @@
   lib,
   pkgs,
   util,
+  inputs,
   ...
 }:
-
 let
+  cfg = config.themes;
+
   file_to_not_import = [
     "default.nix"
   ];
+
+  availableThemes = builtins.map (f: builtins.baseNameOf f) (util.get-import-dir ./. file_to_not_import);
 in
 {
+  imports = lib.mkMerge [
+    (inputs.stylix.homeModules.stylix)
+  ];
+
   options.themes = {
     enable = lib.mkEnableOption "Enable theme module";
+
+    stylix = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+    };
+
     theme = {
       name = lib.mkOption {
-        type = lib.types.str;
+        type = lib.types.enum availableThemes;
         default = "godot";
       };
 
@@ -27,6 +41,21 @@ in
         ];
         default = "dark";
       };
+    };
+  };
+
+
+  config = lib.mkIf cfg.enable {
+    programs.nvim-nix = lib.mkIf (config.programs.nvim != null) {
+      themes.theme = {
+        name = cfg.theme.name;
+        style = cfg.theme.style;
+      };
+    };
+
+    stylix = cfg.stylix {
+      enable = true;
+      base16Scheme = ./. + "/${cfg.theme.name}/base16.yaml";
     };
   };
 }
