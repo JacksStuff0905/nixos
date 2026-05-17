7004c796821ea5ffeeb71d7b8db1b3f0004a9cd7
Jacek Sawiński <jacek.sawinski.0905@gmail.com>
2026-05-18 00:15:10 +0200

    Switched to ssh pubkey for macbook

--- hosts/macbook/host.nix
+++ hosts/macbook/host.nix
@@ -5,7 +5,7 @@
 
     username = "jacek";
 
-    userPubKey = "age1x95fs8qjfs9w70vzuggc50v9tkztlvg77gm9y295yv32yamplf5qp2fzx9";
+    userPubKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC/pGueja9l7ovfuotcmfgtrwnMCu5RL4i2JIewjzJhp";
 
     hostPubKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKbUmyaaN+Q1gCkJVVVdIQro+cPqueFF+Dx4qcNTP0zy";
 
