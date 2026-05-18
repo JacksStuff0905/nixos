fa956afa4200d604fb6a5734f5db2f1392a97197
Jacek Sawiński <jacek.sawinski.0905@gmail.com>
2026-05-18 20:53:02 +0200

    Updated agenix setup

--- hosts/vm/lxc/containers/authelia/host.nix
+++ hosts/vm/lxc/containers/authelia/host.nix
@@ -3,6 +3,8 @@
   host = {
     hostName = "ct-authelia";
 
+    hostPubKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM1ztfwqfrMB4yRnwg0lglDxG24DSC9wA8J8+udQIolo";
+
     isProduction = true;
     isServer = true;
   };
