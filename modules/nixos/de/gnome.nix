269c6e0ec0caf76df7d951d4106521affbd76e30
Jacek Sawiński <jacek.sawinski.0905@gmail.com>
2026-07-26 15:09:15 +0200

    fix(desktop): add xdg portal to gnome

--- modules/nixos/de/gnome.nix
+++ modules/nixos/de/gnome.nix
@@ -1,15 +1,32 @@
-{config, pkgs, lib, ...}:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
   cfg = config.de.gnome;
 in
 {
-	options.de.gnome = {
-		enable = lib.mkEnableOption "Enable gnome module";
-	};
+  options.de.gnome = {
+    enable = lib.mkEnableOption "Enable gnome module";
+  };
 
-	config = lib.mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     services.xserver.enable = true;
 
     services.desktopManager.gnome.enable = true;
-	};
+
+    xdg.portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+      extraPortals = [
+        pkgs.xdg-desktop-portal-gtk
+        pkgs.xdg-desktop-portal-gnome
+      ];
+      config = {
+        common.default = [ "gnome" ];
+      };
+    };
+  };
 }
