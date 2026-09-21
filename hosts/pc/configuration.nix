086191a08b4e76d57ae66969196bb67c54820593
Jacek Sawiński <jacek.sawinski.0905@gmail.com>
2026-09-21 15:47:36 +0200

    chore: enable applyGroup for syncthing in pc

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -105,6 +105,8 @@
   srv.syncthing = {
     enable = true;
 
+    auth.applyGroup = true;
+
     users."${config.host.user.name}" = {
       id = "5XL6TVD-YLV522Y-HF3QXXN-X6NVRC7-E7SBMBY-4OAADUC-BHPR6KM-5YWUMQF";
 
